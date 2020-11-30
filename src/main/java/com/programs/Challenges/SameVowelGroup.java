d268423a6c59cf7c3051f7d7fa5ec10b7d9610cc
binduchowdary856 <binduchowdary856@gmail.com>
2020-11-30 15:43:42 +0530

    SameVowel added

--- src/main/java/com/programs/Challenges/SameVowelGroup.java
+++ src/main/java/com/programs/Challenges/SameVowelGroup.java
@@ -2,6 +2,6 @@ package com.programs.Challenges;
 
 public class SameVowelGroup {
     public static void main(String[] args) {
-            System.out.print("hi");
+            System.out.print("hi gud evng");
     }
 }
